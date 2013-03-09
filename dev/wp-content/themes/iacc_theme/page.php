5a21ef052b37f1b5aa3b828edf8dde647df3c98c
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-03-08 20:54:11 -0500

    Functional Forums

---
+++ dev/wp-content/themes/iacc_theme/page.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Default IACC Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<?php 	get_template_part('part', 'layouts');		
+		global $_theme_layout;
+?>
+
+	<section class="container sbr clearfix">
+
+		<?php get_template_part('part', 'title'); ?>
+
+		<section id="content">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php the_content(); ?>
+
+			<?php endwhile; ?>
+
+		</section><!--/ #content -->
+
+		<?php get_sidebar('side'); ?>
+
+	</section><!--/ .container -->
+
+<?php get_footer(); ?>
