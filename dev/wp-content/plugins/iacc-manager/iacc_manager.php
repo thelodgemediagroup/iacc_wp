684baf19adc8919edea0d96571b572a4a604cf9a
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-04-06 10:22:16 -0400

    Search for valid tx's in iacc manager, display errors if none

--- dev/wp-content/plugins/iacc-manager/iacc_manager.php
+++ dev/wp-content/plugins/iacc-manager/iacc_manager.php
@@ -265,23 +265,31 @@ function imm_get_purchased_events()
 	$purchases = $wpdb->get_results($sql);
 
 	echo '<h2>Purchased Events</h2>';
-	echo '<table class="widefat page fixed"><thead><tr><th>Event</th><th>Event Date</th><th>Ticket Quantity</th><th>Ticket Type</th><th>Member</th><th>Email</th><th>Price</th><th>Purchase Date</th></tr></thead>';
 
-	foreach ($purchases as $purchase)
+	if (!empty($purchases))
 	{
-		echo '<tr>';
-		echo '<td><a href="admin.php?page=purchased_events&action=event_view&event_id='.$purchase->event_id.'" title="View by Event">'.$purchase->event_title.'</a></td>';
-		echo '<td>'.date("F j, Y, g:i a", $purchase->event_date).'</td>';
-		echo '<td>'.$purchase->quantity.'</td>';
-		echo '<td>'.$purchase->ticket_desc.'</td>';
-		echo '<td>'.$purchase->first_name.' '.$purchase->last_name.'</td>';
-		echo '<td>'.$purchase->email.'</td>';
-		echo '<td>'.$purchase->amt.'</td>';
-		echo '<td>'.date("F j, Y, g:i a", $purchase->timestamp).'</td>';
-		echo '</tr>';
-	}
+		echo '<table class="widefat page fixed"><thead><tr><th>Event</th><th>Event Date</th><th>Ticket Quantity</th><th>Ticket Type</th><th>Member</th><th>Email</th><th>Price</th><th>Purchase Date</th></tr></thead>';
 
-	echo '</table>';
+		foreach ($purchases as $purchase)
+		{
+			echo '<tr>';
+			echo '<td><a href="admin.php?page=purchased_events&action=event_view&event_id='.$purchase->event_id.'" title="View by Event">'.$purchase->event_title.'</a></td>';
+			echo '<td>'.date("F j, Y, g:i a", $purchase->event_date).'</td>';
+			echo '<td>'.$purchase->quantity.'</td>';
+			echo '<td>'.$purchase->ticket_desc.'</td>';
+			echo '<td>'.$purchase->first_name.' '.$purchase->last_name.'</td>';
+			echo '<td>'.$purchase->email.'</td>';
+			echo '<td>'.$purchase->amt.'</td>';
+			echo '<td>'.date("F j, Y, g:i a", $purchase->timestamp).'</td>';
+			echo '</tr>';
+		}
+
+		echo '</table>';
+	}
+	else
+	{
+		echo '<p>No events purchased to display</p>';
+	}
 }
 
 function imm_get_registers_by_event()
@@ -315,25 +323,33 @@ function imm_get_registers_by_event()
 function imm_get_membership_upgrades()
 {
 	global $wpdb;
-	$sql = "SELECT * FROM ".MEMBER_PAYPAL." ORDER BY timestamp DESC;";
+	$sql = "SELECT * FROM ".MEMBER_PAYPAL." WHERE tx_state = 1 ORDER BY timestamp DESC;";
 	$members = $wpdb->get_results($sql);
 
 	echo '<h2>Membership Upgrades</h2>';
-	echo '<table class="widefat page fixed"><thead><tr><th>Member Name</th><th>Nickname</th><th>Email</th><th>Membership Type</th><th>Price</th><th>Purchase Date</th></tr></thead>';
 
-	foreach ($members as $member)
+	if (!empty($members))
 	{
-		echo '<tr>';
-		echo '<td>'.$member->first_name.' '.$member->last_name.'</td>';
-		echo '<td>'.$member->nickname.'</td>';
-		echo '<td>'.$member->email.'</td>';
-		echo '<td>'.$member->description.'</td>';
-		echo '<td>'.$member->amt.'</td>';
-		echo '<td>'.date("jS F, Y", $member->timestamp).'</td>';
-		echo '</tr>';
-	}
+		echo '<table class="widefat page fixed"><thead><tr><th>Member Name</th><th>Nickname</th><th>Email</th><th>Membership Type</th><th>Price</th><th>Purchase Date</th></tr></thead>';
 
-	echo '</table>';
+		foreach ($members as $member)
+		{
+			echo '<tr>';
+			echo '<td>'.$member->first_name.' '.$member->last_name.'</td>';
+			echo '<td>'.$member->nickname.'</td>';
+			echo '<td>'.$member->email.'</td>';
+			echo '<td>'.$member->description.'</td>';
+			echo '<td>'.$member->amt.'</td>';
+			echo '<td>'.date("jS F, Y", $member->timestamp).'</td>';
+			echo '</tr>';
+		}
+
+		echo '</table>';
+	}
+	else
+	{
+		echo '<p>No membership purchases to display</p>';
+	}
 }
 
 ?>
